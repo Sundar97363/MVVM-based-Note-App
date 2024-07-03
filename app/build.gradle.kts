4a9b43f724cff621245a2d5e09aed03cfbd6aef1
Mubarak <enpisb251@gmail.com>
2024-07-03 13:59:26 +0530

    Added required dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.kotlinSymbolProcessing)
+    alias(libs.plugins.daggerHilt)
+    id("androidx.navigation.safeargs.kotlin")
+    id("kotlin-parcelize")
 }
 
 android {
@@ -37,12 +41,60 @@ android {
 
 dependencies {
 
+    // Core UI
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // Hilt (Dependency Injection)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+
+    // Room (Local Database)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+
+    // Navigation
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    // ViewModel
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+
+    // Kotlin Ktx (for simplicity delegate's etc,..)
+    implementation(libs.androidx.fragment.ktx)
+
+    // Preference (Data Store)
+    implementation(libs.androidx.datastore.preferences)
+
+    // Coroutines (Asynchronous Task)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Preference Setting Compat (Setting screen)
+    implementation(libs.androidx.preference.ktx)
+
+    // Android 12 Splash
+    implementation(libs.androidx.core.splashscreen)
+
+    //  Testing
     testImplementation(libs.junit)
+    testImplementation(libs.androidx.core.testing)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.androidx.room.testing)
+    testImplementation(libs.androidx.junit.ktx)
+    testImplementation(libs.core.ktx)
+    testImplementation(libs.truth)
+
+
+    // Android Test
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.core.testing)
+    androidTestImplementation(libs.kotlinx.coroutines.test)
+    androidTestImplementation(libs.truth)
 }
